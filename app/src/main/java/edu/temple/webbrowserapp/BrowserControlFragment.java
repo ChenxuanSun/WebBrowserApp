26a1ba6a7d136539c2f868b2354cb4a3f3953695
ChenxuanSun <tug81816@temple.edu>
2020-12-08 12:04:05 +0800

    then I add new brach to this assignment

---
+++ app/src/main/java/edu/temple/webbrowserapp/BrowserControlFragment.java
@@ -0,0 +1,74 @@
+package edu.temple.webbrowserapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
+
+public class BrowserControlFragment extends Fragment {
+    private ImageButton btnNew;
+    private ImageButton btnBookmark;
+    private ImageButton btnSave;
+
+    //interface
+    public void addNewButtonListener(BrowserControlFragment.OnNewButtonClickListener listener){this.listener = listener;}
+
+    public interface OnNewButtonClickListener{
+        void OnNewButtonClick();
+        void OnBookmark();
+        void OnSave();
+    }
+
+    private BrowserControlFragment.OnNewButtonClickListener listener;
+
+    public BrowserControlFragment() {
+        // Required empty public constructor
+    }
+
+    public static BrowserControlFragment newInstance() {
+        return new BrowserControlFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View myFragmentView =
+                inflater.inflate(R.layout.fragment_browser_control, container, false);
+
+        btnNew=myFragmentView.findViewById(R.id.btnAddPage);
+        btnNew.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                listener.OnNewButtonClick();
+            }
+        });
+
+        btnBookmark=myFragmentView.findViewById(R.id.btnBookmark);
+        btnBookmark.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                listener.OnBookmark();
+            }
+        });
+
+        btnSave=myFragmentView.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                listener.OnSave();
+            }
+        });
+
+
+        return myFragmentView;
+    }
+}
